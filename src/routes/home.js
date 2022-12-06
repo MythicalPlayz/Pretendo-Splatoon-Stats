3b9babea826195e7f955b59758cb65e56d84818b
MythicalPlayz <eyaddodo05@gmail.com>
2022-12-06 21:49:18 +0200

    Hide Splatfest if there is no Splatfest

--- src/routes/home.js
+++ src/routes/home.js
@@ -10,11 +10,27 @@ router.get('/', async (request, response) => {
 	const timestartformat = `${timestart.toLocaleDateString(firstlocale)} ${(timestart.getHours() >= 10) ? timestart.getHours() : "0" + timestart.getHours()}:${(timestart.getMinutes() >= 10) ? timestart.getMinutes() : "0" + timestart.getMinutes()}`
 	const timeend = new Date(info[1][3] * 1000)
 	const timeendformat = `${timeend.toLocaleDateString(firstlocale)} ${(timeend.getHours() >= 10) ? timeend.getHours() : "0" + timeend.getHours()}:${(timeend.getMinutes() >= 10) ? timeend.getMinutes() : "0" + timeend.getMinutes()}`
+	
+	avaliable = false
+	Sstatus = ""
+	currentTime =  Date.parse(new Date()) / 1000
+	if (currentTime < info[1][3]){
+		avaliable = true
+		if (currentTime >= info[1][1]) {
+			Sstatus = "Live!"
+		}
+		else if (currentTime >= info[1][0]) {
+			Sstatus = "Happening Soon!"
+		}
+	}
+
     const splatfest = {
 		theme: `${info[0][0]} vs ${info[0][1]}`,
 		timeOfFest: `${timestartformat} - ${timeendformat}`,
 		stages: info[2],
-		gamemode: info[3]
+		gamemode: info[3],
+		avaliable: avaliable,
+		Sstatus: Sstatus
 	}
     
 	const data = {splatfest}
